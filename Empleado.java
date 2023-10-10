8648a3848bbd3a06fac14b9e36a7b4b93f879bcb
PayoCentral <ph836854@gmail.com>
2023-10-10 08:23:32 -0600

    variables empleado

--- Empleado.java
+++ Empleado.java
@@ -1,11 +1,14 @@
 class Empleado {
     private String nombre;
     private String telefono;
+    private int id_empl;
+    private int sueldo;
     private int nss;
 
-    public Trabajador(String nombre, String telefono, String nss) {
+    public Trabajador(String nombre, String telefono, int nss) {
         this.nombre = nombre;
         this.telefono = telefono;
+        
         this.nss = nss;
     }
 
