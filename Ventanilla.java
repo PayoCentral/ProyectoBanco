72bd3c55dfb4dc5bd910a0bebf05d65728d87cf4
Carl0sCru <147523948+Carl0sCru@users.noreply.github.com>
2023-10-12 08:39:13 -0600

    Update Ventanilla.java

--- Ventanilla.java
+++ Ventanilla.java
@@ -1,5 +1,10 @@
 public class Ventanilla extends Empleado{
+  private int numventanilla
 public Ventanilla (String nombre, long telefono,int idempl, Float sueldo, Float salariomin, int nss, int numventanilla){
-super (nombre, telefono, idempl, sueldo, saliaromin, nss)
+  super (nombre, telefono, idempl, sueldo, saliaromin, nss);
+numventanilla= nv;
+}
+public double calcularPago(){
+  return (sueldo-impuesto)*7
 }
 }
