eb46d8628703cab8000ff22f04bda2aa5cf201e5
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-10-12 08:20:06 -0600

    Update Gerente.java
    Cambio 2.0

--- Gerente.java
+++ Gerente.java
@@ -1,17 +1,27 @@
 public class Gerente extends Empleado {
     private int numOficina;
     private int numGerente;
+    private double sueldo; 
+    private double impuesto; 
 
-
-public Gerente(int n, int nn){
+public Gerente(String nombre, long telefono, int n, int nn,
+ double sueldo, double impuesto){
+    super(nombre,telefono); //llamada al constructor de la clase madre 
 numOficina= n; 
 numGerente= nn; 
+this.sueldo=sueldo; 
+this.impuesto=sueldo*0.1; 
 
 }
+
 public int getnumOficina(){
     return numOficina;
 }
 public int getnumGerente(){
     return numGerente; 
+}
+//calcula pago 
+public double calcularPago(){
+    return (sueldo-impuesto)*19;
 }
 }
