8e46835d3ab4a2c4dbbf33ddb2ed94d2058805f3
dieGODdotcom <126695549+dieGODdotcom@users.noreply.github.com>
2023-10-12 08:15:18 -0600

    Create Cuenta.java

---
+++ Cuenta.java
@@ -0,0 +1,25 @@
+public class Cuenta {
+    private String numcuenta;
+    private Float saldo;
+    private String historialtrans;
+    private String moneda;
+
+public Empleado(String numcuenta,Float saldo, String historialtrans, String moneda) {
+this.numcuenta = numcuenta;
+this.saldo = saldo;
+this.historialtrans = historialtrans;
+this.moneda = moneda;
+}
+public String getNumcuenta;{
+    return numcuenta;
+}
+public Float getSaldo;{
+    return saldo;
+}
+public String getHistorialtrans;{
+    return historialtrans;
+}
+public String getMoneda;{
+    return moneda;
+}
+} 
