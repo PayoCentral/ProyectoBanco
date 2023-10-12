d0c33fa129fddd2e65406e87a93c0d5bb07404e8
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-10-12 07:58:17 -0600

    Create Gerente.java
    Creacion de la clase gerente XDDDDD

---
+++ Gerente.java
@@ -0,0 +1,17 @@
+public class Gerente extends Empleado {
+    private int numOficina;
+    private int numGerente;
+
+
+public Gerente(int n, int nn){
+numOficina= n; 
+numGerente= nn; 
+
+}
+public int getnumOficina(){
+    return numOficina;
+}
+public int getnumGerente(){
+    return numGerente; 
+}
+}
