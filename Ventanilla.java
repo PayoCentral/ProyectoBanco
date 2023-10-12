0b498ad9a54899dd202e79d930ae85fb2dde4033
Carl0sCru <147523948+Carl0sCru@users.noreply.github.com>
2023-10-12 08:20:08 -0600

    Ventanilla.java

--- Ventanilla.java
+++ Ventanilla.java
@@ -1,5 +1,5 @@
 public class Ventanilla extends Empleado{
-public Ventanilla (String nombre, String telefono,int idempl, Float sueldo, Float salariomin, int nss, int numventanilla){
+public Ventanilla (String nombre, long telefono,int idempl, Float sueldo, Float salariomin, int nss, int numventanilla){
 super (nombre, telefono, idempl, sueldo, saliaromin, nss)
 }
 }
