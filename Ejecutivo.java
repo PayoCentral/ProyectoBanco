4f73d1b89ee37c65ef551c89aab4047798dadc02
Oscar18900 <147523781+Oscar18900@users.noreply.github.com>
2023-10-12 08:19:06 -0600

    Update Ejecutivo.java

--- Ejecutivo.java
+++ Ejecutivo.java
@@ -1,7 +1,9 @@
-public class Ejecutivo extends Prueba{
+public class Ejecutivo extends Empleado{
 public Ejecutivo (String nombre, String telefono,int idempl, float sueldo, float salariomin, int nss){
 super (nombre, telefono, idempl, 5*salariomin, salariomin, nss);
        //lo de 5 por saliario min seria el sueldo de el ejecutivo
-       
-}
+       }
+       public float calcularSalarioTotal(){
+              return getSueldo();
+        }
 }
