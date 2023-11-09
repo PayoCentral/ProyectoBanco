43a5bad43a75d738e907f9369137fafa72b02921
dieGODdotcom <126695549+dieGODdotcom@users.noreply.github.com>
2023-11-08 22:10:15 -0600

    Create CreditoAutomotriz.java

---
+++ CreditoAutomotriz.java
@@ -0,0 +1,9 @@
+public class CreditoAutomotriz extends Credito(){
+    private Float tasa_interes;
+public CreditoAutomotriz (Float tasa_interes){
+    this.tasa_interes = tasa_interes;
+}
+public Float getTasa_interes(){
+return tasa_interes;
+}
+}
