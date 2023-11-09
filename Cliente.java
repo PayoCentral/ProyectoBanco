4d36a9a924cf75e64756ed13f59861157e5f60e3
Oscar18900 <147523781+Oscar18900@users.noreply.github.com>
2023-11-08 21:25:56 -0600

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,33 @@
+private int numcliente;
+private String nombre;
+private String rfc;
+private String tipo_persona;
+private String fecha_alta;
+
+public Cliente(int numcliente, String nombre, String rfc, String tipo_persona, String fecha_alta){
+this.numcliente=numcliente;
+this.nombre=nombre;
+this.rfc=rfc;
+this.tipo_persona=tipo_persona;
+this.fecha_alta=fecha_alta;
+}
+
+public int getNumcliente(){
+return numcliente;
+}
+
+public String getNombre(){
+return nombre;
+}
+
+public String getRfc(){
+return rfc;
+}
+
+public String getTipo_persona(){
+return tipo_persona;
+}
+
+public String getFecha_alta(){
+return fecha_alta;
+}
