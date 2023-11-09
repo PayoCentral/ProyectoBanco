9ee0c0a41ed852bc24fbae0d98a79cfc2380892d
dieGODdotcom <126695549+dieGODdotcom@users.noreply.github.com>
2023-11-08 22:00:45 -0600

    Create Credito.java

---
+++ Credito.java
@@ -0,0 +1,19 @@
+public class Credito extends Servicios(){
+    private Interger folio;
+    private Float monto;
+    private String fecha_pago;
+public Credito (Interger folio, Float monto, String fecha_pago){
+    this.folio = folio;
+    this.monto = monto;
+    this.fecha_pago = fecha_pago;
+}
+public Interger getFolio(){
+    return folio;
+}
+public Float  getMonto(){
+    return monto;
+}
+public String getFecha_pago(){
+    return fecha_pago;
+}
+}
