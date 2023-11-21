3c24dae9ebc21367baf5b3c14a7c576a7eab7a7e
jessicahercor <147524846+jessicahercor@users.noreply.github.com>
2023-11-20 18:49:49 -0600

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -2,18 +2,13 @@ public class Gerente extends Empleado {
     private int numOficina;
     private int numGerente;
     private float sueldo_gerente;
-    
-public Gerente(){
-    this("", 0, 0, 0f, 0f, 0,0,0f,0);
-}
 
 public Gerente(String nombre,int idempl,  int nss, float salariomin, float impuesto, 
-int numOficina, int numGerente, float sueldo_gerente, int telgerente){
+int numOficina, int numGerente, float sueldo_gerente){
     super(nombre, telefono, idempl, nss, salariomin, impuesto); 
 this.numOficina=numOficina;
 this.numGerente=numGerente;
 this.sueldo_gerente=sueldo_gerente;
-this.telgerente=telgerente;
 
 }
 
