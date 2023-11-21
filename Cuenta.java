57c5232188d5bad3a61209fdadb08b84e273fd28
Oscar18900 <147523781+Oscar18900@users.noreply.github.com>
2023-11-20 22:28:56 -0600

    Update Cuenta.java

--- Cuenta.java
+++ Cuenta.java
@@ -22,4 +22,32 @@ public String getHistorialtrans(){
 public String getMoneda(){
     return moneda;
 }
+        public static String ingresarNumeroCuenta() {
+        return JOptionPane.showInputDialog("Ingrese el número de cuenta:");
+    }
+
+  
+    public static float ingresarSaldoInicial() {
+        return Float.parseFloat(JOptionPane.showInputDialog("Ingrese el saldo inicial:"));
+    }
+
+  
+    public static String ingresarHistorialTransacciones() {
+        return JOptionPane.showInputDialog("Ingrese el historial de transacciones:");
+    }
+
+ 
+    public static String ingresarTipoMoneda() {
+        return JOptionPane.showInputDialog("Ingrese el tipo de moneda:");
+    }
+     public static Cuenta ingresarDatosCuenta() {
+        String numCuenta = ingresarNumeroCuenta();
+        float saldo = ingresarSaldoInicial();
+        String historialTransacciones = ingresarHistorialTransacciones();
+        String moneda = ingresarTipoMoneda();
+        return new Cuenta(numCuenta, saldo, historialTransacciones, moneda);
+    }
+     public void imprimeDatosCuenta(){
+     JOptionPane.showMessageDialog(null, "Numero de la cuenta"+getNumcuenta()+"\nSaldo de la Cuenta "+getSaldo()+" "+getMoneda()+"\nHistorial de transacciones"+getHistorialtrans()+"\nMoneda+ "+getMoneda());
+     }
 } 
