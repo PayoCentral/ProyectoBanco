eb1bc2d54221cb42eed3d237cef8084e3b68da77
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-11-25 18:10:10 -0600

    Update Main.java
    Con Consulta de cuenta, FUNCIONAL

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 public class Main{
     public static void main(String[] args) {
         int opcion = 0;
+        int operacion = 0; 
         do {
             opcion = Integer.parseInt(JOptionPane.showInputDialog(
                 "Seleccione una opción: \n" +
@@ -19,7 +20,38 @@ public class Main{
                 case 2:
                     // Ejecutar la acción de la opción 2
                     System.out.println("Has seleccionado el menú para Clientes");
+                    do{
+                        operacion=Integer.parseInt(JOptionPane.showInputDialog(
+                "Seleccione una opción: \n" +
+                "1. Consulta de cuenta: \n" +
+                "2. Agregar nueva cuenta \n" +
+                "3. Salir"
+            ));
+            switch (operacion) {
+                case 1: 
+                Cliente a= new Cliente(Integer.parseInt(JOptionPane.showInputDialog("Introduce tu número de cuenta")));
+                a.getNumcliente(); 
+          
+                if (a.getNumcliente () == 202322524){
+                        Cliente b= new Cliente(Long.parseLong(JOptionPane.showInputDialog("Introduce tu contraseña"))); 
+                b.getContraseña();
+                          if(b.getContraseña()== 2023){
+                              JOptionPane.showMessageDialog(null, "Ernesto Gonzales Peralta" + "\n" + "Tienes 200 pesos" );
+                          }
+                          else{
+                              JOptionPane.showMessageDialog(null, "La contraseña es incorrecta");
+                          }
+                }
+                else {
+                  JOptionPane.showMessageDialog(null, "Usuario NO existente");
+                }
+                    
                     break;
+            
+                default:
+                    break;
+            }
+            } while (operacion != 3); 
                 case 3:
                 System.out.println("Has seleccionado el menú para uso del Cajero, Caja Multiusos y Pago de Servicios");
                     break;
@@ -27,7 +59,7 @@ public class Main{
                 break;
                 default:
                     JOptionPane.showMessageDialog(null, "Opción no válida");
-            }
-        } while (opcion != 4);
-    }
+        } 
+         } while (opcion != 4);
+}
 }
