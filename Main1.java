6ef8260073c0b9675159259a246541d3ce3b046b
jessicahercor <147524846+jessicahercor@users.noreply.github.com>
2023-11-26 16:47:59 -0600

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,26 +1,19 @@
-public class Main1{
-  public static void main(String[] args) {
-    Gerente gerente1 = new Gerente("Juan Perez", 0001, 502000458, 207.57, 0.17, 0156,015,408.66,222456868);
-    System.out.println(gerente1);
-    System.out.println("El salario de Juan Perez es de " + gerente1.calcularPago());
-  }
-public static void main(String[] args) {
-// Crear objetos de la clase Empleado
-Empleado empleado1 = new Empleado("Juan Perez", "123456789", 1000, 10);
- // Imprimir los datos de los empleados
-  System.out.println(empleado1);
-  // Calcular el salario de los empleados
-  System.out.println("El salario de Juan Perez es de" + empleado1.calcularPago());
-}
-  public static void main( String[] args) {
-    // Crear objetos de la clase Ejecutivo
-  Ejecutivo ejecutivo1 = new Ejecutivo(" Juan Perez", "12345678",1000, 10,"222-234-5467", 1, 101, 12345);
-    // Imprimir los datos del ejecutivo
-  System.out.println(ejecutivo1);
-    // Calcular el salario del ejecutivo
-    System.out.println("El salario de Juan Perez" + ejecutivo1.calcularPago());
-  }
-}
- 
+import javax.swing.JOptionPane;
+public class PincipalTrabajador{
+  public static void main(String[] args){
+    Trabajador[] trabajador = new Trabajador [5];
+    int opc,i,num=0;
+    int continuar;
+    do{
+      do{
+        String op = JOptionPane.showInpuntDialog(" 1:Empleado" + "\n" + " 2:Gerente" + "\n" + " 3:Ejecutivo");
+        opc = Interger.parseInt(op)
+          switch(opc){
+          case 1:
+            trabajador [num] = new Empleado(JOptionPane.showInputDialog("Nombre"),Interger.parseInt(JOptionPane.showInputDialog("Idempl"),Interger.parseInt(JOptionPane.showInputDialog("Nss"),Double.parseDouble(JOptionPane.showInputDialog("Salariomin"),Double.parseDouble(JOptionPane.showInputDialog("Impuesto")));
+             break;
+          case 2: 
+            trabajador [num] = new Gerente(
+  
   
 
