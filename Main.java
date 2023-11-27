551be0ee2a590d7286eb07c2c4ebd5f238f46186
jessicahercor <147524846+jessicahercor@users.noreply.github.com>
2023-11-26 23:00:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,28 +17,28 @@ public class Main{
                     // Ejecutar la acción de la opción 1
                     System.out.println("Has seleccionado el menú para Empleados y Trabajadores del Banco");
                     break;
-                    int opc,i,num=0;
-                    int continuar;
+                    int opc,i,trab=0;
+                    int siguiente;
                 do{
                   do{
         String op = JOptionPane.showInpuntDialog(" 1:Empleado" + "\n" + " 2:Gerente" + "\n" + " 3:Ejecutivo");
         opc = Interger.parseInt(op)
-          switch(opc){
+          switch(operacion){
           case 1:
-            trabajador [num] = new Empleado(JOptionPane.showInputDialog("Nombre"),Interger.parseInt(JOptionPane.showInputDialog("Idempl"),Interger.parseInt(JOptionPane.showInputDialog("Nss"),Double.parseDouble(JOptionPane.showInputDialog("Salariomin"),Double.parseDouble(JOptionPane.showInputDialog("Impuesto"))))));
+            trabajador [trab] = new Empleado(JOptionPane.showInputDialog("Nombre"),Interger.parseInt(JOptionPane.showInputDialog("Idempl"),Interger.parseInt(JOptionPane.showInputDialog("Nss"),Double.parseDouble(JOptionPane.showInputDialog("Salariomin"),Double.parseDouble(JOptionPane.showInputDialog("Impuesto"))))));
              break;
           case 2: 
-            trabajador [num] = new Gerente(Interger.parseInt(JOptionPane.showInputDialog("NumOficina"),Interger.parseInt(JOptionPane.showInputDialog("NumGerente"),Double.parseDouble(JOptionPane.showInpuntDialog("SueldoGerente")))));
+            trabajador [trab] = new Gerente(Interger.parseInt(JOptionPane.showInputDialog("NumOficina"),Interger.parseInt(JOptionPane.showInputDialog("NumGerente"),Double.parseDouble(JOptionPane.showInpuntDialog("SueldoGerente")))));
             break;
             case 3:
-            trabajador [num] = new Ejecutivo(Interger.parseInt(JOptionPane.showInpuntDialog("NumOficina"),Interger.parseInt(JOptionPane.showInpuntDialog("NumEjecutivo"),Double.parseDouble(JOptionPane.showInpuntDialog("SueldoEjecutivo")))));
+            trabajador [trab] = new Ejecutivo(Interger.parseInt(JOptionPane.showInpuntDialog("NumOficina"),Interger.parseInt(JOptionPane.showInpuntDialog("NumEjecutivo"),Double.parseDouble(JOptionPane.showInpuntDialog("SueldoEjecutivo")))));
             break;
             default:
             JOptionPane.showMessageDialog(null,"Opcion Incorrecta");
             }
             } while((opc < 1) && (opc < 2) && (opc < 3));
             num = num+1;
-            continuar = JOptionPane.showConfirmDialog(null,"¿Desea añadir otro Trabajador?","Recabando informacion",JOptionPane.YES_NO_OPTION);
+            continuar = JOptionPane.showConfirmDialog(null,"¿Desea añadir otro Trabajador?","Generando información",JOptionPane.YES_NO_OPTION);
         } while
         (continuar==JOptionPane.YES_NO_OPTION);
         for (i=0;i<num;i++)
