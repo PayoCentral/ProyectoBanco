07ac453f2d0b5a09138ba957655177d33d00d0d7
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-11-26 22:51:52 -0600

    Update Gerente.java
    Corrección de errores, colocar métodos get y set

--- Gerente.java
+++ Gerente.java
@@ -2,15 +2,16 @@ public class Gerente extends Empleado{
 private int numOficina;
     private int numGerente;
     private float sueldo_gerente;
+    private int telgerente; 
     public Gerente(){
     this("", 0, 0, 0f, 0f, 0,0,0f,0);
     }
 public Gerente(String nombre,int idempl,  int nss, float salariomin, float impuesto, 
 int numOficina, int numGerente, float sueldo_gerente,int telgerente){
-    super(nombre, telefono, idempl, nss, salariomin, impuesto); 
+    super(nombre, idempl, nss, salariomin, impuesto); 
 this.numOficina=numOficina;
 this.numGerente=numGerente;
 this.sueldo_gerente=sueldo_gerente;
 this.telgerente=telgerente;
 }
-}   
+}  
