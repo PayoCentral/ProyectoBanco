c093de4075af466a9e2542bcd61e7dfe5c57d0f8
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-11-30 18:12:44 -1000

    Add files via upload

---
+++ ProyectoFinal/CajeroConRetiros.java
@@ -0,0 +1,38 @@
+public class CajeroConRetiros extends Cajero {
+    private int[] retiros;
+    private int contadorRetiros;
+    
+
+         public CajeroConRetiros(double saldo) {
+                super(saldo);
+                this.saldo=saldo; 
+                this.retiros = new int[10]; // Por ejemplo, un array de 10 retiros
+                this.contadorRetiros = 0;
+    }
+
+    public double getSaldo(){
+        return saldo; 
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo=saldo; 
+    }
+
+    public void realizarRetiro(int monto) {
+        if (monto > 0 && monto <= getSaldo()) {
+            setSaldo(getSaldo() - monto);
+            if (contadorRetiros < retiros.length) {
+                retiros[contadorRetiros] = monto;
+                contadorRetiros++;
+            } else {
+                System.out.println("No se pueden agregar más retiros. El array está lleno.");
+            }
+        } else {
+            System.out.println("Saldo insuficiente para realizar el retiro de $" + monto);
+        }
+    }
+
+    public int[] obtenerRetiros() {
+        return retiros;
+    }
+}
