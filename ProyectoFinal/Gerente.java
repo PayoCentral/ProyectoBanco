698a035adb168c528eddb10f7ee36bbaa51565df
Kippie01PW <146268821+Kippie01PW@users.noreply.github.com>
2023-11-30 18:11:00 -1000

    Add files via upload

---
+++ ProyectoFinal/Gerente.java
@@ -0,0 +1,29 @@
+public class Gerente extends Empleado{
+private int numOficina;
+    private int numGerente;
+    private float sueldo_gerente;
+    private int telgerente; 
+    public Gerente(){
+    this("", 0, 0, 0f, 0f, 0,0,0f,0);
+    }
+public Gerente(String nombre,int idempl,  int nss, float salariomin, float impuesto, 
+int numOficina, int numGerente, float sueldo_gerente,int telgerente){
+    super(nombre, idempl, nss, salariomin, impuesto); 
+this.numOficina=numOficina;
+this.numGerente=numGerente;
+this.sueldo_gerente=sueldo_gerente;
+this.telgerente=telgerente;
+}
+    public int getnumOficina(){
+    return numOficina;
+}
+public int getnumGerente(){
+    return numGerente; 
+}
+    public float getsueldo_gerente(){
+        return sueldo_gerente;
+    }
+    public int gettelgerente(){
+        return telgerente;
+    }
+}  
